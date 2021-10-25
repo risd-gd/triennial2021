18fff0bc96ae6d49061fbb71a1837b7cd0b238ce
zscheinfeld <77294209+zscheinfeld@users.noreply.github.com>
2021-10-25 10:18:33 -0400

    Add files via upload

---
+++ triennial2021/script.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  let constrain =3500;
+  let mouseOverContainer = document.getElementById("ex1");
+  let ex1Layer = document.getElementById("ex1-layer");
+
+  function transforms(x, y, el) {
+    let box = el.getBoundingClientRect();
+    let calcX = -(y - box.y - (box.height / 2)) / (constrain * 0.8);
+    let calcY = (x - box.x - (box.width / 2)) / (constrain * 0.8);
+    
+    return "perspective(100px) "
+      + "   rotateX("+ calcX*9 +"deg) "
+      + "   rotateY("+ calcY*9 +"deg) ";
+  };
+
+  function transformElement(el, xyEl) {
+    el.style.transform  = transforms.apply(null, xyEl);
+  }
+
+  mouseOverContainer.onmousemove = function(e) {
+    let xy = [e.clientX, e.clientY];
+    let position = xy.concat([ex1Layer]);
+
+    window.requestAnimationFrame(function(){
+      transformElement(ex1Layer, position);
+    });
+  };
+
+});
+
+var x = 0 
+
+setInterval(function changeClass(){ 
+  x = x + 1 
+
+  if (x == 0){
+    document.getElementById('textchecker').style.fontFamily = 'hHalfFold'
+    console.log("change1")
+  }
+
+  if (x == 1){
+    document.getElementById('textchecker').style.fontFamily = 'quadFold'
+    console.log("change2")
+  }
+
+  if (x == 2){
+    document.getElementById('textchecker').style.fontFamily = 'triFold'
+    console.log("change3")
+  }
+
+  if (x == 3){
+    document.getElementById('textchecker').style.fontFamily = 'vHalfFold'
+    console.log("change3")
+  }
+
+  if (x == 4){
+    document.getElementById('textchecker').style.fontFamily = 'Checker'
+    console.log("change3")
+  }
+
+  if (x>4){
+    x = 0
+  }
+
+}, 500);
+
+
+
